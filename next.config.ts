9b1adaf507bb871e35a470eb69bdf4b611307fe8
Thomas Nguyen <latiosthinh@live.com>
2025-03-01 22:50:05 +0700

    udp: config proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://jacksonsfencing-configcommerce-d-cl.niteco.dev/api/:path*"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
